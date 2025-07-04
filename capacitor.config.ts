f0fa4643b66154e3b5892fd9ab38377afbb18199
Carlos Garcia <josecarlosgarciadiaz@gmail.com>
2025-07-04 10:15:25 -0600

    actualizacion para implementacion de app movil de ticket

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ticketsumae25.app',
+  appName: 'tickets umae25',
+  webDir: 'dist/tickets/browser'
+};
+
+export default config;
