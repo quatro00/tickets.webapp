a9166cc8c1346bd27cd09520fdd4a56d54a77332
Carlos Garcia <josecarlosgarciadiaz@gmail.com>
2025-07-06 00:27:17 -0600

    upd

--- src/app/modules/supervisor/home/home.component.ts
+++ src/app/modules/supervisor/home/home.component.ts
@@ -1,8 +1,40 @@
-import { Component } from '@angular/core';
+import { CurrencyPipe, NgClass } from '@angular/common';
+import {
+    ChangeDetectionStrategy,
+    Component,
+    OnDestroy,
+    OnInit,
+    ViewEncapsulation,
+} from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatRippleModule } from '@angular/material/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatTableModule } from '@angular/material/table';
+import { MatTabsModule } from '@angular/material/tabs';
+import { Router } from '@angular/router';
+import { TranslocoModule } from '@jsverse/transloco';
+import { ApexOptions, NgApexchartsModule } from 'ng-apexcharts';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [
+    TranslocoModule,
+        MatIconModule,
+        MatButtonModule,
+        MatRippleModule,
+        MatMenuModule,
+        MatTabsModule,
+        MatButtonToggleModule,
+        NgApexchartsModule,
+        MatTableModule,
+        NgClass,
+        CurrencyPipe,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
